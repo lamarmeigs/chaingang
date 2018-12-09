5110892c5e86dfd5d105d40c3f369ee36eaf64f1
Lamar <lamar@divvydose.com>
2018-12-09 14:28:07 -0600

    Build command to create models from corpora

--- chaingang.py
+++ chaingang.py
@@ -1,33 +1,61 @@
 import argparse
+import re
+import sys
+
+import markovify
 
 
 def read_corpus(path, ignore_pattern=None):
-    pass
+    with open(path) as f:
+        corpus = f.read()
+    if ignore_pattern:
+        corpus = re.sub(ignore_pattern, '', corpus)
+    return corpus
 
 
-def build_model(*corpora, weights=None):
-    pass
+def build_model(corpora, weights=None):
+    models = [markovify.Text(corpus, retain_original=False) for corpus in corpora]
+    model = markovify.combine(models, weights)
+    return model
 
 
 def write_model(model, path=None):
-    pass
+    serialized_model = model.to_json()
+    if path:
+        with open(path, 'w') as f:
+            f.write(serialized_model)
+    else:
+        sys.stdout.write(serialized_model)
 
 
 def load_model(path):
     pass
 
 
-def create_model(args):
+def create_model(args, parser):
     """Create a new Markov model from the given command-line arguments."""
-    pass
+    # TODO: allow command to read from stdin
+    corpora = (read_corpus(path, args.pattern) for path in args.corpora)
+    if args.weights and len(args.weights) != len(args.corpora):
+        parser.error('Number of weights must match number of corpora')
+    model = build_model(corpora, args.weights)
+    write_model(model, path=args.outfile)
 
 
-def generate_sentences(args):
+def generate_sentences(args, parser):
     """Generate sentences from models in the given command-line arguments."""
     pass
 
 
-if __name__ == '__main':
+def _compile_regex(pattern):
+    try:
+        pattern = re.compile(pattern)
+    except re.error:
+        raise argparse.ArgumentTypeError(f'Invalid regex: {pattern}')
+    return pattern
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Play with Markov chains.')
     subparsers = parser.add_subparsers()
 
@@ -38,15 +66,23 @@ if __name__ == '__main':
         '-c',
         '--corpora',
         nargs='+',
+        required=True,
         help='Path to files containing the corpora to model',
     )
     model_parser.add_argument(
         '-w',
         '--weights',
         nargs='*',
+        type=int,
         help='Relative weights to apply to each corpus',
     )
-    model_parser.add_argument('-d', dest='pattern', help='Pattern to ignore in corpus')
+    model_parser.add_argument(
+        '-d',
+        '--ignore-pattern',
+        dest='pattern',
+        type=_compile_regex,
+        help='Pattern to ignore in corpora'
+    )
     model_parser.add_argument(
         '-o',
         '--outfile',
@@ -64,11 +100,14 @@ if __name__ == '__main':
     )
     generate_parser.add_argument(
         '-n',
-        '--num_sentences',
+        '--num-sentences',
         type=int,
         help='Number of setences to generate'
     )
 
-    # Parse & run
+    # Parse, validate, and run
     args = parser.parse_args()
-    args.run_command(args)
+    if not hasattr(args, 'run_command'):
+        parser.print_usage()
+        parser.exit()
+    args.run_command(args, parser)
